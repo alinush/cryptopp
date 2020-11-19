ff409f79b05fab71d3a649ffcc71792df13c5dc0
Alin Tomescu <alinush@mit.edu>
2020-11-19 12:26:36 -0800

    RSA and ed25519 benchmarks only

--- bench3.cpp
+++ bench3.cpp
@@ -317,67 +317,67 @@ void Benchmark3(double t, double hertz)
 	std::cout << "\n<THEAD style=\"background: #F0F0F0\">";
 	std::cout << "\n<TR><TH>Operation<TH>Milliseconds/Operation" << mco;
 
-	std::cout << "\n<TBODY style=\"background: white;\">";
-	{
-		BenchMarkCrypto<RSAES<OAEP<SHA1> > >("TestData/rsa1024.dat", "RSA 1024", t);
-		BenchMarkCrypto<LUCES<OAEP<SHA1> > >("TestData/luc1024.dat", "LUC 1024", t);
-		BenchMarkCrypto<DLIES<> >("TestData/dlie1024.dat", "DLIES 1024", t);
-		BenchMarkCrypto<LUC_IES<> >("TestData/lucc512.dat", "LUCELG 512", t);
-	}
+	//std::cout << "\n<TBODY style=\"background: white;\">";
+	//{
+	//	BenchMarkCrypto<RSAES<OAEP<SHA1> > >("TestData/rsa1024.dat", "RSA 1024", t);
+	//	BenchMarkCrypto<LUCES<OAEP<SHA1> > >("TestData/luc1024.dat", "LUC 1024", t);
+	//	BenchMarkCrypto<DLIES<> >("TestData/dlie1024.dat", "DLIES 1024", t);
+	//	BenchMarkCrypto<LUC_IES<> >("TestData/lucc512.dat", "LUCELG 512", t);
+	//}
 
-	std::cout << "\n<TBODY style=\"background: yellow;\">";
-	{
-		BenchMarkCrypto<RSAES<OAEP<SHA1> > >("TestData/rsa2048.dat", "RSA 2048", t);
-		BenchMarkCrypto<LUCES<OAEP<SHA1> > >("TestData/luc2048.dat", "LUC 2048", t);
-		BenchMarkCrypto<DLIES<> >("TestData/dlie2048.dat", "DLIES 2048", t);
-		BenchMarkCrypto<LUC_IES<> >("TestData/lucc1024.dat", "LUCELG 1024", t);
-	}
+	//std::cout << "\n<TBODY style=\"background: yellow;\">";
+	//{
+		//BenchMarkCrypto<RSAES<OAEP<SHA1> > >("TestData/rsa2048.dat", "RSA 2048", t);
+		//BenchMarkCrypto<LUCES<OAEP<SHA1> > >("TestData/luc2048.dat", "LUC 2048", t);
+		//BenchMarkCrypto<DLIES<> >("TestData/dlie2048.dat", "DLIES 2048", t);
+		//BenchMarkCrypto<LUC_IES<> >("TestData/lucc1024.dat", "LUCELG 1024", t);
+	//}
 
-	std::cout << "\n<TBODY style=\"background: white;\">";
-	{
-		BenchMarkSignature<RSASS<PSSR, SHA1> >("TestData/rsa1024.dat", "RSA 1024", t);
-		BenchMarkSignature<RWSS<PSSR, SHA1> >("TestData/rw1024.dat", "RW 1024", t);
-		BenchMarkSignature<LUCSS<PSSR, SHA1> >("TestData/luc1024.dat", "LUC 1024", t);
-		BenchMarkSignature<NR<SHA1> >("TestData/nr1024.dat", "NR 1024", t);
-		BenchMarkSignature<DSA>("TestData/dsa1024.dat", "DSA 1024", t);
-		BenchMarkSignature<LUC_HMP<SHA1> >("TestData/lucs512.dat", "LUC-HMP 512", t);
-		BenchMarkSignature<ESIGN<SHA1> >("TestData/esig1023.dat", "ESIGN 1023", t);
-		BenchMarkSignature<ESIGN<SHA1> >("TestData/esig1536.dat", "ESIGN 1536", t);
-	}
+	//std::cout << "\n<TBODY style=\"background: white;\">";
+	//{
+		//BenchMarkSignature<RSASS<PSSR, SHA1> >("TestData/rsa1024.dat", "RSA 1024", t);
+		//BenchMarkSignature<RWSS<PSSR, SHA1> >("TestData/rw1024.dat", "RW 1024", t);
+		//BenchMarkSignature<LUCSS<PSSR, SHA1> >("TestData/luc1024.dat", "LUC 1024", t);
+		//BenchMarkSignature<NR<SHA1> >("TestData/nr1024.dat", "NR 1024", t);
+		//BenchMarkSignature<DSA>("TestData/dsa1024.dat", "DSA 1024", t);
+		//BenchMarkSignature<LUC_HMP<SHA1> >("TestData/lucs512.dat", "LUC-HMP 512", t);
+		//BenchMarkSignature<ESIGN<SHA1> >("TestData/esig1023.dat", "ESIGN 1023", t);
+		//BenchMarkSignature<ESIGN<SHA1> >("TestData/esig1536.dat", "ESIGN 1536", t);
+	//}
 
 	std::cout << "\n<TBODY style=\"background: yellow;\">";
 	{
 		BenchMarkSignature<RSASS<PSSR, SHA1> >("TestData/rsa2048.dat", "RSA 2048", t);
-		BenchMarkSignature<RWSS<PSSR, SHA1> >("TestData/rw2048.dat", "RW 2048", t);
-		BenchMarkSignature<LUCSS<PSSR, SHA1> >("TestData/luc2048.dat", "LUC 2048", t);
-		BenchMarkSignature<NR<SHA1> >("TestData/nr2048.dat", "NR 2048", t);
-		BenchMarkSignature<LUC_HMP<SHA1> >("TestData/lucs1024.dat", "LUC-HMP 1024", t);
-		BenchMarkSignature<ESIGN<SHA1> >("TestData/esig2046.dat", "ESIGN 2046", t);
+		//BenchMarkSignature<RWSS<PSSR, SHA1> >("TestData/rw2048.dat", "RW 2048", t);
+		//BenchMarkSignature<LUCSS<PSSR, SHA1> >("TestData/luc2048.dat", "LUC 2048", t);
+		//BenchMarkSignature<NR<SHA1> >("TestData/nr2048.dat", "NR 2048", t);
+		//BenchMarkSignature<LUC_HMP<SHA1> >("TestData/lucs1024.dat", "LUC-HMP 1024", t);
+		//BenchMarkSignature<ESIGN<SHA1> >("TestData/esig2046.dat", "ESIGN 2046", t);
 	}
 
-	std::cout << "\n<TBODY style=\"background: white;\">";
-	{
-		BenchMarkKeyAgreement<XTR_DH>("TestData/xtrdh171.dat", "XTR-DH 171", t);
-		BenchMarkKeyAgreement<XTR_DH>("TestData/xtrdh342.dat", "XTR-DH 342", t);
-		BenchMarkKeyAgreement<DH>("TestData/dh1024.dat", "DH 1024", t);
-		BenchMarkKeyAgreement<DH>("TestData/dh2048.dat", "DH 2048", t);
-		BenchMarkKeyAgreement<LUC_DH>("TestData/lucd512.dat", "LUCDIF 512", t);
-		BenchMarkKeyAgreement<LUC_DH>("TestData/lucd1024.dat", "LUCDIF 1024", t);
-		BenchMarkKeyAgreement<MQV>("TestData/mqv1024.dat", "MQV 1024", t);
-		BenchMarkKeyAgreement<MQV>("TestData/mqv2048.dat", "MQV 2048", t);
+	//std::cout << "\n<TBODY style=\"background: white;\">";
+	//{
+	//	BenchMarkKeyAgreement<XTR_DH>("TestData/xtrdh171.dat", "XTR-DH 171", t);
+	//	BenchMarkKeyAgreement<XTR_DH>("TestData/xtrdh342.dat", "XTR-DH 342", t);
+	//	BenchMarkKeyAgreement<DH>("TestData/dh1024.dat", "DH 1024", t);
+	//	BenchMarkKeyAgreement<DH>("TestData/dh2048.dat", "DH 2048", t);
+	//	BenchMarkKeyAgreement<LUC_DH>("TestData/lucd512.dat", "LUCDIF 512", t);
+	//	BenchMarkKeyAgreement<LUC_DH>("TestData/lucd1024.dat", "LUCDIF 1024", t);
+	//	BenchMarkKeyAgreement<MQV>("TestData/mqv1024.dat", "MQV 1024", t);
+	//	BenchMarkKeyAgreement<MQV>("TestData/mqv2048.dat", "MQV 2048", t);
 
 #if 0
-		BenchMarkKeyAgreement<ECHMQV160>("TestData/hmqv160.dat", "HMQV P-160", t);
-		BenchMarkKeyAgreement<ECHMQV256>("TestData/hmqv256.dat", "HMQV P-256", t);
-		BenchMarkKeyAgreement<ECHMQV384>("TestData/hmqv384.dat", "HMQV P-384", t);
-		BenchMarkKeyAgreement<ECHMQV512>("TestData/hmqv512.dat", "HMQV P-512", t);
+	//	BenchMarkKeyAgreement<ECHMQV160>("TestData/hmqv160.dat", "HMQV P-160", t);
+	//	BenchMarkKeyAgreement<ECHMQV256>("TestData/hmqv256.dat", "HMQV P-256", t);
+	//	BenchMarkKeyAgreement<ECHMQV384>("TestData/hmqv384.dat", "HMQV P-384", t);
+	//	BenchMarkKeyAgreement<ECHMQV512>("TestData/hmqv512.dat", "HMQV P-512", t);
 
-		BenchMarkKeyAgreement<ECFHMQV160>("TestData/fhmqv160.dat", "FHMQV P-160", t);
-		BenchMarkKeyAgreement<ECFHMQV256>("TestData/fhmqv256.dat", "FHMQV P-256", t);
-		BenchMarkKeyAgreement<ECFHMQV384>("TestData/fhmqv384.dat", "FHMQV P-384", t);
-		BenchMarkKeyAgreement<ECFHMQV512>("TestData/fhmqv512.dat", "FHMQV P-512", t);
+	//	BenchMarkKeyAgreement<ECFHMQV160>("TestData/fhmqv160.dat", "FHMQV P-160", t);
+	//	BenchMarkKeyAgreement<ECFHMQV256>("TestData/fhmqv256.dat", "FHMQV P-256", t);
+	//	BenchMarkKeyAgreement<ECFHMQV384>("TestData/fhmqv384.dat", "FHMQV P-384", t);
+	//	BenchMarkKeyAgreement<ECFHMQV512>("TestData/fhmqv512.dat", "FHMQV P-512", t);
 #endif
-	}
+	//}
 
 	std::cout << "\n<TBODY style=\"background: yellow;\">";
 	{
@@ -388,62 +388,62 @@ void Benchmark3(double t, double hertz)
 		BenchMarkSigning("ed25519", sign, t);
 		BenchMarkVerification("ed25519", sign, verify, t);
 		BenchMarkKeyGen("x25519", agree, t);
-		BenchMarkAgreement("x25519", agree, t);
+		//BenchMarkAgreement("x25519", agree, t);
 	}
 
-	std::cout << "\n<TBODY style=\"background: white;\">";
-	{
-		ECIES<ECP>::Decryptor cpriv(Test::GlobalRNG(), ASN1::secp256k1());
-		ECIES<ECP>::Encryptor cpub(cpriv);
-		ECDSA<ECP, SHA1>::Signer spriv(cpriv);
-		ECDSA<ECP, SHA1>::Verifier spub(spriv);
-		ECDSA_RFC6979<ECP, SHA1>::Signer spriv2(cpriv);
-		ECDSA_RFC6979<ECP, SHA1>::Verifier spub2(spriv);
-		ECGDSA<ECP, SHA1>::Signer spriv3(Test::GlobalRNG(), ASN1::secp256k1());
-		ECGDSA<ECP, SHA1>::Verifier spub3(spriv3);
-		ECDH<ECP>::Domain ecdhc(ASN1::secp256k1());
-		ECMQV<ECP>::Domain ecmqvc(ASN1::secp256k1());
+	//std::cout << "\n<TBODY style=\"background: white;\">";
+	//{
+	//	ECIES<ECP>::Decryptor cpriv(Test::GlobalRNG(), ASN1::secp256k1());
+	//	ECIES<ECP>::Encryptor cpub(cpriv);
+	//	ECDSA<ECP, SHA1>::Signer spriv(cpriv);
+	//	ECDSA<ECP, SHA1>::Verifier spub(spriv);
+	//	ECDSA_RFC6979<ECP, SHA1>::Signer spriv2(cpriv);
+	//	ECDSA_RFC6979<ECP, SHA1>::Verifier spub2(spriv);
+	//	ECGDSA<ECP, SHA1>::Signer spriv3(Test::GlobalRNG(), ASN1::secp256k1());
+	//	ECGDSA<ECP, SHA1>::Verifier spub3(spriv3);
+	//	ECDH<ECP>::Domain ecdhc(ASN1::secp256k1());
+	//	ECMQV<ECP>::Domain ecmqvc(ASN1::secp256k1());
 
-		BenchMarkEncryption("ECIES over GF(p) 256", cpub, t);
-		BenchMarkDecryption("ECIES over GF(p) 256", cpriv, cpub, t);
-		BenchMarkSigning("ECDSA over GF(p) 256", spriv, t);
-		BenchMarkVerification("ECDSA over GF(p) 256", spriv, spub, t);
-		BenchMarkSigning("ECDSA-RFC6979 over GF(p) 256", spriv2, t);
-		BenchMarkVerification("ECDSA-RFC6979 over GF(p) 256", spriv2, spub2, t);
-		BenchMarkSigning("ECGDSA over GF(p) 256", spriv3, t);
-		BenchMarkVerification("ECGDSA over GF(p) 256", spriv3, spub3, t);
-		BenchMarkKeyGen("ECDHC over GF(p) 256", ecdhc, t);
-		BenchMarkAgreement("ECDHC over GF(p) 256", ecdhc, t);
-		BenchMarkKeyGen("ECMQVC over GF(p) 256", ecmqvc, t);
-		BenchMarkAgreement("ECMQVC over GF(p) 256", ecmqvc, t);
-	}
+	//	BenchMarkEncryption("ECIES over GF(p) 256", cpub, t);
+	//	BenchMarkDecryption("ECIES over GF(p) 256", cpriv, cpub, t);
+	//	BenchMarkSigning("ECDSA over GF(p) 256", spriv, t);
+	//	BenchMarkVerification("ECDSA over GF(p) 256", spriv, spub, t);
+	//	BenchMarkSigning("ECDSA-RFC6979 over GF(p) 256", spriv2, t);
+	//	BenchMarkVerification("ECDSA-RFC6979 over GF(p) 256", spriv2, spub2, t);
+	//	BenchMarkSigning("ECGDSA over GF(p) 256", spriv3, t);
+	//	BenchMarkVerification("ECGDSA over GF(p) 256", spriv3, spub3, t);
+	//	BenchMarkKeyGen("ECDHC over GF(p) 256", ecdhc, t);
+	//	BenchMarkAgreement("ECDHC over GF(p) 256", ecdhc, t);
+	//	BenchMarkKeyGen("ECMQVC over GF(p) 256", ecmqvc, t);
+	//	BenchMarkAgreement("ECMQVC over GF(p) 256", ecmqvc, t);
+	//}
 
-	std::cout << "\n<TBODY style=\"background: yellow;\">";
-	{
-		ECIES<EC2N>::Decryptor cpriv(Test::GlobalRNG(), ASN1::sect233r1());
-		ECIES<EC2N>::Encryptor cpub(cpriv);
-		ECDSA<EC2N, SHA1>::Signer spriv(cpriv);
-		ECDSA<EC2N, SHA1>::Verifier spub(spriv);
-		ECDSA_RFC6979<EC2N, SHA1>::Signer spriv2(cpriv);
-		ECDSA_RFC6979<EC2N, SHA1>::Verifier spub2(spriv);
-		ECGDSA<EC2N, SHA1>::Signer spriv3(Test::GlobalRNG(), ASN1::sect233r1());
-		ECGDSA<EC2N, SHA1>::Verifier spub3(spriv3);
-		ECDH<EC2N>::Domain ecdhc(ASN1::sect233r1());
-		ECMQV<EC2N>::Domain ecmqvc(ASN1::sect233r1());
+	//std::cout << "\n<TBODY style=\"background: yellow;\">";
+	//{
+		//ECIES<EC2N>::Decryptor cpriv(Test::GlobalRNG(), ASN1::sect233r1());
+		//ECIES<EC2N>::Encryptor cpub(cpriv);
+		//ECDSA<EC2N, SHA1>::Signer spriv(cpriv);
+		//ECDSA<EC2N, SHA1>::Verifier spub(spriv);
+		//ECDSA_RFC6979<EC2N, SHA1>::Signer spriv2(cpriv);
+		//ECDSA_RFC6979<EC2N, SHA1>::Verifier spub2(spriv);
+		//ECGDSA<EC2N, SHA1>::Signer spriv3(Test::GlobalRNG(), ASN1::sect233r1());
+		//ECGDSA<EC2N, SHA1>::Verifier spub3(spriv3);
+		//ECDH<EC2N>::Domain ecdhc(ASN1::sect233r1());
+		//ECMQV<EC2N>::Domain ecmqvc(ASN1::sect233r1());
 
-		BenchMarkEncryption("ECIES over GF(2^n) 233", cpub, t);
-		BenchMarkDecryption("ECIES over GF(2^n) 233", cpriv, cpub, t);
-		BenchMarkSigning("ECDSA over GF(2^n) 233", spriv, t);
-		BenchMarkVerification("ECDSA over GF(2^n) 233", spriv, spub, t);
-		BenchMarkSigning("ECDSA-RFC6979 over GF(2^n) 233", spriv2, t);
-		BenchMarkVerification("ECDSA-RFC6979 over GF(2^n) 233", spriv2, spub2, t);
-		BenchMarkSigning("ECGDSA over GF(2^n) 233", spriv3, t);
-		BenchMarkVerification("ECGDSA over GF(2^n) 233", spriv3, spub3, t);
-		BenchMarkKeyGen("ECDHC over GF(2^n) 233", ecdhc, t);
-		BenchMarkAgreement("ECDHC over GF(2^n) 233", ecdhc, t);
-		BenchMarkKeyGen("ECMQVC over GF(2^n) 233", ecmqvc, t);
-		BenchMarkAgreement("ECMQVC over GF(2^n) 233", ecmqvc, t);
-	}
+		//BenchMarkEncryption("ECIES over GF(2^n) 233", cpub, t);
+		//BenchMarkDecryption("ECIES over GF(2^n) 233", cpriv, cpub, t);
+		//BenchMarkSigning("ECDSA over GF(2^n) 233", spriv, t);
+		//BenchMarkVerification("ECDSA over GF(2^n) 233", spriv, spub, t);
+		//BenchMarkSigning("ECDSA-RFC6979 over GF(2^n) 233", spriv2, t);
+		//BenchMarkVerification("ECDSA-RFC6979 over GF(2^n) 233", spriv2, spub2, t);
+		//BenchMarkSigning("ECGDSA over GF(2^n) 233", spriv3, t);
+		//BenchMarkVerification("ECGDSA over GF(2^n) 233", spriv3, spub3, t);
+		//BenchMarkKeyGen("ECDHC over GF(2^n) 233", ecdhc, t);
+		//BenchMarkAgreement("ECDHC over GF(2^n) 233", ecdhc, t);
+		//BenchMarkKeyGen("ECMQVC over GF(2^n) 233", ecmqvc, t);
+		//BenchMarkAgreement("ECMQVC over GF(2^n) 233", ecmqvc, t);
+	//}
 
 	std::cout << "\n</TABLE>" << std::endl;
 }
